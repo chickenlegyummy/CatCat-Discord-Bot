c9d47cda815d77e6fc69cd7cce3bd06c849d93ac
chickenlegyummy <163871645+chickenlegyummy@users.noreply.github.com>
2025-05-08 23:33:10 +0800

    feat: added bot activity status in dc

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, ActivityType } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] }); // Client Instance
@@ -27,6 +27,10 @@ for (const folder of commandFolders) {
 
 client.once(Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	client.user.setActivity({
+		name: 'Meow!',
+		type: ActivityType.Playing,
+	})
 });
 
 client.login(token); // Login with client's token
